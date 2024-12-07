c02fd25d51e3950ec2d816b0bec89d596f081711
2lag <96544487+2lag@users.noreply.github.com>
2024-12-06 22:47:14 -0600

    touch more of learning

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@v0.171.0/build/three.module.js";
+import * as THREE from "three";
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
 
+let cam;
+let scene;
+let renderer;
+let controls;
 function init( ) {
-  const scene = new THREE.Scene( );
-  const cam = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  const renderer = new THREE.WebGLRenderer( );
-  
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  if ( !WebGL.isWebGL2Available( ) )
+    return false;
+
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+
+  scene = new THREE.Scene( );
+  cam = new THREE.PerspectiveCamera( 69, w / h, 0.1, 2000 );
+  renderer = new THREE.WebGLRenderer( );
+  renderer.setSize( w, h );
   document.body.appendChild( renderer.domElement );
-  cam.position.z = 5;
+  cam.position.set( 0, 0, 5 );
 
   const geometry = new THREE.BoxGeometry( );
   const material = new THREE.MeshBasicMaterial( { color: 0xE76969 } );
   const cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
 
-  function animate( ) {
-    requestAnimationFrame( animate );
+  const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry( 100, 100 ),
+    new THREE.MeshBasicMaterial( { color: 0x808080, side: THREE.DoubleSide } )
+  );
+  ground.rotation.x = -Math.PI / 2;
+  scene.add( ground );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+  controls = new FirstPersonControls( cam, renderer.domElement );
+  controls.movementSpeed = 5;
+  controls.lookSpeed = 0.1;
 
-    renderer.render( scene, cam );
-  }
-
-  animate( );
+  return true;
 }
 
-init( );
+function render( ) {
+  requestAnimationFrame( render );
+  controls.update( 0.016 ); // 1 / 60
+  renderer.render( scene, cam );
+}
+
+if ( init( ) )
+  render( );
+else document.getElementsByTagName( 'body' )[ 0 ].appendChild( WebGL.getWebGL2ErrorMessage( ) );
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-https://threejs.org/docs/#manual/en/introduction/Installation
-https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene
-
-
-Use `Live Server` VSC extension to run locally w/o CORS issue
+clean up current code
+load some shit
+make some cool shit
+move on
